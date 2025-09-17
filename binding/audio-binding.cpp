6f4ae87edaa170f4a46f823f6a4d548e33c3e5aa
Haru <harukwork@gmail.com>
2025-09-17 13:34:58 -0400

    Add in boolean identifier to method head

--- binding/audio-binding.cpp
+++ binding/audio-binding.cpp
@@ -98,7 +98,7 @@ RB_METHOD_GUARD(audio_bgmPlay)
     double pos = 0.0;
     VALUE track = Qnil;
 	bool fadeIn = true;
-    rb_get_args(argc, argv, "z|iifo", &filename, &volume, &pitch, &pos, &track, &fadeIn RB_ARG_END);
+    rb_get_args(argc, argv, "z|iifob", &filename, &volume, &pitch, &pos, &track, &fadeIn RB_ARG_END);
     shState->audio().bgmPlay(filename, volume, pitch, pos, MAYBE_NIL_TRACK(track), fadeIn);
     return Qnil;
 }
