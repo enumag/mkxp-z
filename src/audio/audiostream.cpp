Eblo/mkxp-z-eulogy

Make BGM fadein optional

src/audio/audiostream.cpp
@@ -76,7 +76,8 @@ AudioStream::~AudioStream()
 void AudioStream::play(const std::string &filename,
                        int volume,
                        int pitch,
-                       float offset)
+                       float offset,
+					   bool doFadein)
 {
 	finiFadeOutInt();
 
@@ -143,7 +144,7 @@ void AudioStream::play(const std::string &filename,
 	setVolume(Base, _volume);
 	stream.setPitch(_pitch);
 
-	if (offset > 0)
+	if (offset > 0 && doFadein)
 	{
 		setVolume(FadeIn, 0);
 		startFadeIn();

src/audio/audiostream.h
@@ -127,7 +127,8 @@ struct AudioStream
 	void play(const std::string &filename,
 	          int volume,
 	          int pitch,
-	          float offset = 0);
+	          float offset = 0,
+			  bool doFadein = true);
 	void stop();
 	void fadeOut(int duration);
 	void seek(float offset);

enumag/mkxp-z

src/audio/audiostream.cpp
@@ -76,7 +76,8 @@ AudioStream::~AudioStream()
 void AudioStream::play(const std::string &filename,
                        int volume,
                        int pitch,
-                       double offset)
+                       double offset,
+					   bool doFadein)
 {
 	finiFadeOutInt();
 
@@ -138,7 +139,7 @@ void AudioStream::play(const std::string &filename,
 	setVolume(Base, _volume);
 	stream.setPitch(_pitch);
 
-	if (offset > 0)
+	if (offset > 0 && doFadein)
 	{
 		setVolume(FadeIn, 0);
 		startFadeIn();

src/audio/audiostream.h
@@ -127,7 +127,8 @@ struct AudioStream
 	void play(const std::string &filename,
 	          int volume,
 	          int pitch,
-	          double offset = 0);
+	          double offset = 0,
+			  bool doFadein = true);
 	void stop();
 	void fadeOut(int duration);
 	void seek(double offset);